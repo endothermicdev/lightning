719f5dcd50982de412f4988a103c62ea04a44006
Alex Myers <alex@endothermic.dev>
2023-07-25 09:44:24 -0500

    gossipd: add back in malformed own node-announcement warning

--- gossipd/routing.c
+++ gossipd/routing.c
@@ -1956,6 +1956,12 @@ u8 *handle_node_announcement(struct routing_state *rstate, const u8 *node_ann,
 		 *    - MAY close the connection.
 		 *    - MUST NOT process the message further.
 		 */
+		if (node_id_eq(&node_id, &rstate->daemon->id)) {
+			u8 *warn = towire_warningfmt(rstate, NULL,
+						     "Malformed node_announcement %s",
+						     tal_hex(tmpctx, node_ann));
+			return warn;
+		}
 		/* FIXME: We removed a warning about the
 		 * node_announcement being malformed since the warning
 		 * could cause lnd to disconnect (seems they treat
